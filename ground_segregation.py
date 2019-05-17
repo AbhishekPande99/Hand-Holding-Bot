5e0f88efa38bd9187608c5a244bf7913b3eec3a2
Abhishek Pande <39085138+AbhishekPande99@users.noreply.github.com>
2019-05-17 10:15:04 +0530

    Update ground_segregation.py

--- ground_segregation.py
+++ ground_segregation.py
@@ -68,15 +68,7 @@ def segregate(label,strip_size,size1,size2):
     ch[ch!=mode]=-1
     ch[ch==mode]=255
     ch=np.clip(ch,0,255)
-def segregate(label,strip_size,size1,size2):
-    from scipy import stats
-    ch=np.array(label)   
-    ch=ch.reshape([size1,size2])
-    x=ch[size1-strip_size:]
-    mode=stats.mode(x.reshape([-1]))[0][0]
-    ch[ch!=mode]=-1
-    ch[ch==mode]=255
-    ch=np.clip(ch,0,255)
+
 
 size1=200
 size2=200
